05e4ef6f041176c1e41eea7c3ce8c9ceebaa4628
Mario Rodriguez <mario.rodriguez35@yahoo.com>
2019-04-07 04:03:15 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_reader :name, :type, :booty
+  @@all = []
+
+  def initialize(name, type, booty)
+    @name = name 
+    @type = type
+    @booty = booty
+    @@ships << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
